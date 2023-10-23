42ec60406e2d1487806d2d1ef48c30a4b25ba2e9
Will <doggy.huang@gmail.com>
2023-10-24 01:05:50 +0800

    0.2.0 - 2023-10-24
    * 加入 URL 格式判斷，避免 URL 也被加入盤古之白

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the **盤古之白** will be documented in this file.
 
+## 0.2.0 - 2023-10-24
+
+* 加入 URL 格式判斷，避免 URL 也被加入盤古之白
+
 ## 0.1.1 - 2023-10-22
 
 * 在 `README.md` 加入「盤古之白」
--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,110 @@
-import path = require("path");
+import { pangu } from './Pangu';
+import path = require('path');
 import vscode = require('vscode');
-import pangu = require('pangu');
 
 export function activate(ctx: vscode.ExtensionContext) {
-	ctx.subscriptions.push(vscode.commands.registerCommand('pangu2.add_space_selection', addSpaceSelection));
-	ctx.subscriptions.push(vscode.commands.registerCommand('pangu2.add_space_whole', addSpaceWholeDocument));
-	ctx.subscriptions.push(new Watcher());
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      'pangu2.add_space_selection',
+      addSpaceSelection
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      'pangu2.add_space_whole',
+      addSpaceWholeDocument
+    )
+  );
+  ctx.subscriptions.push(new Watcher());
 }
 
-export function deactivate() { }
+export function deactivate() {}
 
-function addSpace(e: vscode.TextEditor, d: vscode.TextDocument, sel: vscode.Selection[]) {
-	e.edit(function (edit: vscode.TextEditorEdit) {
-		for (var x = 0; x < sel.length; x++) {
-			let txt: string = d.getText(new vscode.Range(sel[x].start, sel[x].end));
-			let parsed: string = pangu.spacing(txt);
-			if (txt !== parsed) {
-				edit.replace(sel[x], parsed);
-			}
-		}
-	});
+function addSpace(
+  e: vscode.TextEditor,
+  d: vscode.TextDocument,
+  sel: vscode.Selection[]
+) {
+  e.edit(function (edit: vscode.TextEditorEdit) {
+    for (var x = 0; x < sel.length; x++) {
+      let txt: string = d.getText(new vscode.Range(sel[x].start, sel[x].end));
+      let parsed: string = pangu.spacing(txt);
+      if (txt !== parsed) {
+        edit.replace(sel[x], parsed);
+      }
+    }
+  });
 }
 function addSpaceSelection() {
-	let e = vscode.window.activeTextEditor;
-	if (e) {
-		let d = e.document;
-		let sels = e.selections;
-		addSpace(e, d, [...sels]);
-	}
+  let e = vscode.window.activeTextEditor;
+  if (e) {
+    let d = e.document;
+    let sels = e.selections;
+    addSpace(e, d, [...sels]);
+  }
 }
 
 function addSpaceWholeDocument() {
-	let e = vscode.window.activeTextEditor;
-	if (e) {
-		let d = e.document;
-		let sel = new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(Number.MAX_VALUE, Number.MAX_VALUE));
-		addSpace(e, d, [sel]);
-	}
+  let e = vscode.window.activeTextEditor;
+  if (e) {
+    let d = e.document;
+    let sel = new vscode.Selection(
+      new vscode.Position(0, 0),
+      new vscode.Position(Number.MAX_VALUE, Number.MAX_VALUE)
+    );
+    addSpace(e, d, [sel]);
+  }
 }
 
 class Watcher {
-	private _disposable!: vscode.Disposable;
-	private _config!: vscode.WorkspaceConfiguration;
-	private _whitelist: Array<string> = [];
+  private _disposable!: vscode.Disposable;
+  private _config!: vscode.WorkspaceConfiguration;
+  private _whitelist: Array<string> = [];
 
-	public getConfig() {
-		this._config = vscode.workspace.getConfiguration('pangu2');
-	}
+  public getConfig() {
+    this._config = vscode.workspace.getConfiguration('pangu2');
+  }
 
-	constructor() {
-		this.getConfig();
+  constructor() {
+    this.getConfig();
 
-		let subscriptions: vscode.Disposable[] = [];
-		this._disposable = vscode.Disposable.from(...subscriptions);
-		vscode.workspace.onDidSaveTextDocument(this._onDidSaveDoc, this, subscriptions);
+    let subscriptions: vscode.Disposable[] = [];
+    this._disposable = vscode.Disposable.from(...subscriptions);
+    vscode.workspace.onDidSaveTextDocument(
+      this._onDidSaveDoc,
+      this,
+      subscriptions
+    );
 
-		vscode.workspace.onDidChangeConfiguration(event => {
-			let affected = event.affectsConfiguration("pangu2");
-			if (affected) {
-				this.getConfig();
-			}
-		})
-	}
+    vscode.workspace.onDidChangeConfiguration((event) => {
+      let affected = event.affectsConfiguration('pangu2');
+      if (affected) {
+        this.getConfig();
+      }
+    });
+  }
 
-	dispose() {
-		this._disposable.dispose();
-	}
+  dispose() {
+    this._disposable.dispose();
+  }
 
-	_onDidSaveDoc(e: vscode.TextDocument) {
-		if (this._config.get('autoAddSpaceOnSave', false)) {
-			var ext = path.extname(e.fileName).toLowerCase();
-			var ext_without_dot = ext.startsWith('.') ? ext.substring(1) : ext;
+  _onDidSaveDoc(e: vscode.TextDocument) {
+    if (this._config.get('autoAddSpaceOnSave', false)) {
+      var ext = path.extname(e.fileName).toLowerCase();
+      var ext_without_dot = ext.startsWith('.') ? ext.substring(1) : ext;
 
-			this._whitelist = this._config.get('autoAddSpaceOnSaveFileExtensions', [ ".txt" ]);
-			this._whitelist = this._whitelist.map(i => i.toLowerCase());
+      this._whitelist = this._config.get('autoAddSpaceOnSaveFileExtensions', [
+        '.txt',
+      ]);
+      this._whitelist = this._whitelist.map((i) => i.toLowerCase());
 
-			if (this._whitelist.includes('*') || this._whitelist.includes(ext) || this._whitelist.includes(ext_without_dot)) {
-				addSpaceWholeDocument();
-			}
-		}
-	}
+      if (
+        this._whitelist.includes('*') ||
+        this._whitelist.includes(ext) ||
+        this._whitelist.includes(ext_without_dot)
+      ) {
+        addSpaceWholeDocument();
+      }
+    }
+  }
 }
-
